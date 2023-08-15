b3fe810f45310cbcebad0d1b1981d8983cee2c8b
TechGeno <harshasri910@gmail.com>
2023-08-15 15:22:17 +0530

    Debugs done, added some styling-2

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from .views import Home
 from django.conf import settings
 from django.conf.urls.static import static
-
+# from django.contrib import admin
 urlpatterns = [
     path('',Home.as_view(),name="home"),
+    # path('admin/',admin.site.urls),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
